db800d1854a60f49c6bc7450896257c773354938
Ionut Gradinaru <ionut.gradinaru@assist.ro>
2022-09-18 08:03:50 +0300

    Create the Spring Boot application and add the .gitignore file

---
+++ src/main/java/com/ionutgradinaru/xlsx/XlsxImportApplication.java
@@ -0,0 +1,13 @@
+package com.ionutgradinaru.xlsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsxImportApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XlsxImportApplication.class, args);
+  }
+
+}
